25364236f7def81001b024b19b095bb1e9d564a5
E K <emmanuelkoupoh@gmail.com>
2022-07-27 16:39:45 +0000

    ready to run test

---
+++ tests/gradio_project/test_demo_app.py
@@ -0,0 +1,16 @@
+# importation of the solution function to test
+from gradio_project.demo_app import greet
+import pandas as pd
+
+
+def test_solution_valid_inputs():
+    """This is the test for the implemented solution passing
+    valid inputs"""
+    valid_inputs = ["Emmanuel", "Alain"]
+    expected_outputs = ["Hello Emmanuel!!", "Hello Alain!!"]
+    outputs = []
+
+    for input in valid_inputs:
+        outputs.append(greet(input))
+
+    assert outputs == expected_outputs
